87bbe2ed3a469190c9ab5b0a13bc958cda3c7172
Farouk-Osman <faroukosman53@gmail.com>
2025-08-18 16:03:18 +0300

    Update the server.jds file

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.DB_URI)
+.then(() => console.log('MongoDB connected'))
+.catch(err => console.error('MongoDB connection error:', err));
 dotenv.config({ path: './config.env' });
+const port = process.env.PORT;
 const app = express();
-const port = process.env.PORT || 3000;
+app.use(morgan(process.env.NODE_ENV === 'development' ? 'dev' : 'combined'));
+console.log(`Environment: ${process.env.NODE_ENV}`);
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
