36f8ffdb16b76e2d5980eaac6463ff52fcab1f14
Farouk-Osman <faroukosman53@gmail.com>
2025-09-14 15:33:25 +0300

    Add category population to product retrieval queries

--- models/productModel.js
+++ models/productModel.js
@@ -85,5 +85,20 @@ const productSchema = new mongoose.Schema({
 },   { timestamps: true }
 );
 
+productSchema.pre(/^find/, function (next) {
+  this.populate({
+    path: 'category',
+    select: 'name',
+  });
+  next();
+});
+
+productSchema.pre(/^findOne/, function (next) {
+  this.populate({
+    path: 'category',
+    select: 'name',
+  });
+  next();
+});
 
 module.exports = mongoose.model('Product', productSchema);
